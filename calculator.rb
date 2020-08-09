9cea72400b7b3602d24c669ec7a6b05d3730d5d7
Andre Pierre <andre.laurence94@gmail.com>
2020-08-09 18:14:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,8 @@
 # Add your variables here
 
-first_number = 0
-second_number = 1
+first_number = 1
+second_number = 0
+
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
